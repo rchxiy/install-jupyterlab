5224e4dd6b3d9ec79c12d1e0b31e89e22cf3198f
litbrother <128768844+ichinur@users.noreply.github.com>
2024-12-25 15:53:50 +0700

    Update jupy-auto-install.sh

--- jupy-auto-install.sh
+++ jupy-auto-install.sh
@@ -36,7 +36,7 @@ node -v
 npm -v
 
 echo "Installing JupyterLab..."
-pip install --user jupyterlab
+pip install --user --break-system-packages jupyterlab
 
 echo "Configuring PATH and PS1 in .bashrc..."
 BASHRC_PATH="$HOME/.bashrc"
